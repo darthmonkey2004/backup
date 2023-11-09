13dfd041e5cde6270c158708a6fcd350e66c7dd3
darthmonkey2004 <darthmonkey2004@users.noreply.github.com>
2023-11-09 16:11:44 -0600

    added install.sh
    Added installer to install dependencies, download repo, and install desktop launcher.

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkdesktop() {
+	echo "[Desktop Entry]" > backup.desktop
+	echo "Version=1.0" >> backup.desktop
+	echo "Name=Backup Utility" >> backup.desktop
+	echo "Comment=Backup utility using duplicity and python3." >> backup.desktop
+	echo "Exec=backup.py --menu" >> backup.desktop
+	echo "Path=/home/$USER/.local" >> backup.desktop
+	echo "Icon=/home/$USER/.local/backup.png" >> backup.desktop
+	echo "Terminal=false" >> backup.desktop
+	echo "Type=Application" >> backup.desktop
+	echo "Categories=Utility" >> backup.desktop
+	echo "StartupWMClass=GUI" >> backup.desktop
+	mv backup.desktop "$HOME/.local/share/applications/backup.desktop"
+}
+
+setup() {
+	#test for git
+	hasgit=$(which git)
+	if [ -z "$hasgit" ]; then
+		#install if needed
+		sudo apt-get install -y git
+	fi
+	#clone repo
+	git clone "https://github.com/darthmonkey2004/backup.git"
+	cd backup
+	#create source distro
+	python3 setup.py sdist
+	#install package using requirements.txt
+	package=$(ls dist | grep "tar.gz")
+	pip3 install -r requirements.txt "dist/$package"
+	#copy icon to user folder (for .desktop)
+	cp backup.png "$HOME/.local/backup.png"
+	mkdesktop
+}
+
+if [ ! -d "$HOME/.backups" ]; then
+	cd "$HOME"
+	setup
+else
+	mkdesktop
+fi
